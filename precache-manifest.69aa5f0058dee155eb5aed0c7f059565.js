a075b2a4ccf26d4484e7d488099d9bd611be28e8
Benjamin Candy <bcandyman93@gmail.com>
2020-02-26 20:48:26 -0800

    Updates

---
+++ precache-manifest.69aa5f0058dee155eb5aed0c7f059565.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2cfa1ac643795ef1a7bf834f248b179",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "7e21f48f29cedd5acef6",
+    "url": "/Clicky/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1a1ba8a5091a7173a0d7",
+    "url": "/Clicky/static/css/main.69fc9a34.chunk.css"
+  },
+  {
+    "revision": "7e21f48f29cedd5acef6",
+    "url": "/Clicky/static/js/2.bc3702df.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky/static/js/2.bc3702df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1ba8a5091a7173a0d7",
+    "url": "/Clicky/static/js/main.c5a6eb31.chunk.js"
+  },
+  {
+    "revision": "7df6600b39aadb68c324",
+    "url": "/Clicky/static/js/runtime-main.2a3e645b.js"
+  },
+  {
+    "revision": "1973cdec6d9f8fd764ceeb080be54199",
+    "url": "/Clicky/static/media/Bart_Simpson.1973cdec.png"
+  },
+  {
+    "revision": "beb0d37a8155b2f73ae417173ac7146e",
+    "url": "/Clicky/static/media/Bugs_Bunny.beb0d37a.png"
+  },
+  {
+    "revision": "12733528b7f8f81e79ce7c254da73b0c",
+    "url": "/Clicky/static/media/Foghorn_Leghorn.12733528.png"
+  },
+  {
+    "revision": "c109fa07b92018818b417b05a8bf0458",
+    "url": "/Clicky/static/media/Fred_Flinstone.c109fa07.png"
+  },
+  {
+    "revision": "a41e40191573eb60fd0676dcd00af702",
+    "url": "/Clicky/static/media/Homer_Simpson.a41e4019.png"
+  },
+  {
+    "revision": "9d939552ef615f666a01c30c0a30c4a9",
+    "url": "/Clicky/static/media/Jerry.9d939552.png"
+  },
+  {
+    "revision": "5cbf8fe2ee58268e0bb2f82f9bb9a2d0",
+    "url": "/Clicky/static/media/Marvin.5cbf8fe2.png"
+  },
+  {
+    "revision": "a8669b5d358b26793b89a65650d7a14d",
+    "url": "/Clicky/static/media/Peter_Griffin.a8669b5d.png"
+  },
+  {
+    "revision": "30031b6afa174a3ad2723d8f32be0f6b",
+    "url": "/Clicky/static/media/Pink_Panther.30031b6a.png"
+  },
+  {
+    "revision": "dd440f3edd868b116a7ed64548d95f9d",
+    "url": "/Clicky/static/media/Popeye.dd440f3e.png"
+  },
+  {
+    "revision": "6eed493d843eeed06e5bd0ab6259d9c1",
+    "url": "/Clicky/static/media/Scooby.6eed493d.png"
+  },
+  {
+    "revision": "2346adc1ce57af2bf22b6182cf4bb982",
+    "url": "/Clicky/static/media/Wile_E_Coyote.2346adc1.png"
+  }
+]);
